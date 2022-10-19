14b4257e4629e7d5f1c6ab73dad0eadb624cae5d
Jose Palacio <jose.palacio@uniandes.edu.co>
2022-10-19 13:24:23 -0500

    Se agrega funcionalidad de conversion

--- tareas/tareas.py
+++ tareas/tareas.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from pydub import AudioSegment
 
 celery_app = Celery(__name__, broker = 'redis://localhost:6379/0')
 
@@ -6,3 +7,9 @@ celery_app = Celery(__name__, broker = 'redis://localhost:6379/0')
 def registrar_log(usuario, fecha):
     with open('log_signin.txt', 'a+') as file:
         file.write('{} - inicio de sesion: {}\n'.format(usuario, fecha))
+
+@celery_app.task(name = 'convertir_audio')
+def convert_music(origin_path, dest_path, new_format):
+    sound = AudioSegment.from_mp3(origin_path)
+    sound.export(dest_path, format = new_format)
+    print ('\n-> El archivo {}, se convirtio a : {}'.format(origin_path, new_format))
