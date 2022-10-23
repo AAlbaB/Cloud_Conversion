1b5208ca33810ef09372aef0c62a8c8431dce9d7
AndresAlba <o.alba@uniandes.edu.co>
2022-10-23 18:14:38 -0500

    Desactiva envio de correo electronico

--- Backend/tareas.py
+++ Backend/tareas.py
@@ -20,16 +20,16 @@ def registrar_log(usuario, fecha):
 def convert_music(origin_path, dest_path, origin_format, new_format, name_file, task_id):
 
     new_task = session.query(File).get(task_id)
-    user_id = new_task.user
-    user = session.query(User).get(user_id)
-    email_user = user.email
+    #user_id = new_task.user
+    #user = session.query(User).get(user_id)
+    #email_user = user.email
 
     if origin_format == "mp3":
         sound = AudioSegment.from_mp3(origin_path)
         sound.export(dest_path, format = new_format)
         print ('\n-> El audio {}, se convirtio a : {}'.format(name_file, new_format))
         new_task.status = 'processed'
-        envio_correo(email_user, name_file, new_format)
+        #envio_correo(email_user, name_file, new_format)
         session.commit()
 
     elif origin_format == "ogg":
@@ -37,7 +37,7 @@ def convert_music(origin_path, dest_path, origin_format, new_format, name_file, 
         sound.export(dest_path, format = new_format)
         print ('\n-> El audio {}, se convirtio a : {}'.format(name_file, new_format))
         new_task.status = 'processed'
-        envio_correo(email_user, name_file, new_format)
+        #envio_correo(email_user, name_file, new_format)
         db.session.commit()
 
     elif origin_format == "wav":
@@ -45,12 +45,13 @@ def convert_music(origin_path, dest_path, origin_format, new_format, name_file, 
         sound.export(dest_path, format = new_format)
         print ('\n-> El audio {}, se convirtio a : {}'.format(name_file, new_format))
         new_task.status = 'processed'
-        envio_correo(email_user, name_file, new_format)
+        #envio_correo(email_user, name_file, new_format)
         db.session.commit()
 
     else:
         print ('Ocurrio un error en la conversion con el archivo {}'.format(name_file))
 
+# Se desactiva envio den correo para pruebas de carga y estres
 def envio_correo(corre_usuario, name_file, new_format):
     connection = smtp.SMTP_SSL('smtp.gmail.com', 465)
     email_addr = 'mailtotestandes@gmail.com'
